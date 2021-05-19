a814317916b38af39bacd1257c9b9e6ab76c995b
sash9696 <67851308+sash9696@users.noreply.github.com>
2021-05-19 21:07:39 +0530

    Add files via upload

---
+++ pointvsarr.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[]={10,20,30};
+    int *p1=arr;
+    int(*p2)[3]=&arr;
+    cout<<*p1<<"\n";
+    cout<<**p2<<"\n";
+    
+    return 0;
+}
