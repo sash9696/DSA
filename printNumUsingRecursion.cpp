f9fdbbf7579cbc0e8ecc5b8b871f62d3ca067494
sash9696 <67851308+sash9696@users.noreply.github.com>
2021-05-29 13:19:35 +0530

    Add files via upload

---
+++ printNumUsingRecursion.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+void fun(int n)
+{	
+	if(n==0)
+		return ;
+	else
+	{
+		fun(n-1);
+		cout<<n<<" ";
+		
+		
+	}
+}
+int main()
+{ 	
+	fun(5);
+	return 0;
+
+
+}
+	
