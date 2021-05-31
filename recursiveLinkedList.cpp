29b6cca4ea060a9831ebfccdf1facb01a84fbdbd
sash9696 <67851308+sash9696@users.noreply.github.com>
2021-05-31 18:58:43 +0530

    Add files via upload

---
+++ recursiveLinkedList.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+
+	int data;
+	Node *next;
+
+	Node(int x){
+		data=x;
+		next=NULL;
+	}
+};
+ void printList(Node *head){
+	Node *curr =  head;
+	if(curr==NULL){
+		return;
+	}
+	else(curr!=NULL);
+	{	
+		cout<< curr -> data <<" ";
+		printList(curr -> next);
+
+	}
+}	
+int main(){
+
+	Node *head = new Node(10);
+	Node *temp1 = new Node(20);
+	Node *temp2 = new Node(30);
+
+	head -> next = temp1;
+	temp1 -> next = temp2;
+	printList(head);
+
+	return 0;
+}
