c33d61a2222759aec751e1d86b622c6296751b03
sash9696 <67851308+sash9696@users.noreply.github.com>
2021-06-18 21:59:42 +0530

    Add files via upload

---
+++ gcd.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int gcd (int a, int b) {
+	if (a==0) {
+		return b;
+	}
+	return gcd((b%a), a);
+}
+
+int main(){
+	int a = 10;
+	int b = 15;
+	cout<<gcd(a,b)<<endl;
+	return 0;
+}
