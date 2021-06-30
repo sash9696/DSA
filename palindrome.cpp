3623040bdca7051a0dc0da971b3a530bf48c256f
sash9696 <67851308+sash9696@users.noreply.github.com>
2021-06-30 22:36:55 +0530

    Add files via upload

--- palindrome.cpp
+++ palindrome.cpp
@@ -1,30 +1,39 @@
-#include <iostream>
+#include <iostream> 
+
 using namespace std;
 
+bool isPalindrome(char array[], int size) {
+	int low=0;
+	int high=size-1;
+
+	while(low<high) {
+		if (array[low] == array[high]) {
+			low++;
+			high--;
+		}
+		else {
+			cout<<"false hai\n";
+			return false;
+		}
+		
+	}
+	cout<<"true hai\n";
+	return true;
 
 
-int main()
-{
-       string s;
-       cin>>s;
-       
-       int start=0;
-       int end=s.length()-1;
-       
-       while(start<end){
-           if(s[start] !=s[end])
-           {
-               cout<<"No";
-               
-           }
-           else{
-               start++;
-               end --;
-           }
-        
-       }
-       cout<<"Yes";
-        return 0;
-       
+}
 
+int main () {
+	int n;
+	cout<<"Enter size: \n";
+	cin>>n;
+	char arr[n];
+	cout<<"Enter characters: \n";
+	for (int i=0; i<n; i++) {
+		cin>>arr[i];
+	}
+
+	isPalindrome(arr, n);
+
+	return 0;
 }
