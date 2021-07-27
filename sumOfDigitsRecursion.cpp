d7a8308309ef906d789603de5a140a12f7989387
sash9696 <67851308+sash9696@users.noreply.github.com>
2021-07-27 23:01:30 +0530

    Add files via upload

--- sumOfDigitsRecursion.cpp
+++ sumOfDigitsRecursion.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
+
 using namespace std;
 
-int getSum(int n){
-	if(n==0)
-	{
+int sumOfDigits (int n) {
+	if (n==0) 
 		return 0;
-	}
-	return getSum(n/10)+n%10;
+	return ((n%10) + sumOfDigits(n/10));
 }
-int main()
-{
-	cout<<getSum(876);
+
+int main() {
+	int n;
+	cin>>n;
+	cout<<sumOfDigits(n);
 	return 0;
 }
