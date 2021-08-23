32016801308b4aa01709d59b5f7f582da8661859
sash9696 <67851308+sash9696@users.noreply.github.com>
2021-08-23 15:19:33 +0530

    Add files via upload

--- arrayLatgestElement.cpp
+++ arrayLatgestElement.cpp
@@ -6,20 +6,21 @@ int largeElement(int arr[], int n)
 	int res=0;
 	for(int i=0; i<=n; i++)
 	{
+	    
 		if(arr[i]>arr[res])
 		{
 			res = i;
 		}
 	} 
-	cout<<arr[res]<<endl;
-	return res;
+	 
+	 return arr[res];
 
 
 }
 int main()
 {	
-	int arr[]={5, 8, 20, 10};
-	int n = 4;
+	int arr[]={1,1,};
+	int n = 2;
 	cout<< largeElement(arr, n)<<endl;
 	return 0;
 }
