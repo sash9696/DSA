d6d17b333862df8303d0a169bce2a7489023a3f5
sash9696 <67851308+sash9696@users.noreply.github.com>
2022-01-11 23:22:55 +0530

    Add files via upload

---
+++ for loop.cpp
@@ -0,0 +1,23 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{ 
+    int i=0; 
+      
+    for (i = 1; i <= 10; i++) 
+    { 
+        std::cout <<( "Hello World\n");     
+    } 
+  
+    return 0; 
+} 
