a07c20d8e60c1eaebc14422475f94003b5418cff
sash9696 <67851308+sash9696@users.noreply.github.com>
2022-01-16 23:20:43 +0530

    Add files via upload

---
+++ displayArray.cpp
@@ -0,0 +1,33 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int numbers[5] = {7, 5, 6, 12, 35};
+
+    cout << "The numbers are: ";
+
+    //  Printing array elements
+    // using range based for loop
+    for (const int &n : numbers) {
+        cout << n << "  ";
+    }
+
+
+    cout << "\nThe numbers are: ";
+
+    //  Printing array elements
+    // using traditional for loop
+    for (int i = 0; i < 5; ++i) {
+        cout << numbers[i] << "  ";
+    }
+
+    return 0;
+}
